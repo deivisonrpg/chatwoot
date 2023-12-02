254bf9fb687443d24116f60d987c1c3637e3c993
Deivison Lincoln <deivisonrpg@hotmail.com>
2023-12-02 14:59:15 -0300

    Simplicaficado SEEDS

--- lib/seeders/account_seeder.rb
+++ lib/seeders/account_seeder.rb
@@ -48,7 +48,7 @@ class Seeders::AccountSeeder
 
   def set_up_users
     @account_data['users'].each do |user|
-      user_record = User.create_with(name: user['name'], password: 'Password1!.').find_or_create_by!(email: (user['email']).to_s)
+      user_record = User.create_with(name: user['name'], password: '123456').find_or_create_by!(email: (user['email']).to_s)
       user_record.skip_confirmation!
       user_record.save!
       Avatar::AvatarFromUrlJob.perform_later(user_record, "https://ui-avatars.com/api/?background=random")
